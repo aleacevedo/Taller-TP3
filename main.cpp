cba9634f2fab7b8f67a5051abab8b964a9f2e2d9
alejoacevedo <ale.acevedo@live.com.ar>
2019-09-28 18:47:23 -0300

    Add test for directories

--- main.cpp
+++ main.cpp
@@ -2,16 +2,26 @@
 
 #include "honey_pot.h"
 #include "file_error.h"
+#include "directory.h"
 #include "key_not_found_error.h"
+#include "dir_exist_error.h"
+#include "dir_not_exist_error.h"
 
 int main(int argc, char* argv[]) {
   try {
-    HoneyPot pot("entradas/config.cfg");
-    std::cout << pot.get_current_dir();
-    std::cout << pot.get_msg_unknown_cmd();
+    Directory dir;
+    dir.add("First directory");
+    dir.add("Second directory");
+    std::cout << dir.list();
+    dir.remove("First dire ctory");
+    std::cout << dir.list();
   } catch (FileError &e) {
     std::cout << e.what();
   } catch (KeyNotFoundError &e) {
+    std::cout << e.what();
+  } catch(DirNotExistError &e) {
+    std::cout << e.what();
+  } catch(DirExistError &e) {
     std::cout << e.what();
   }
 }
