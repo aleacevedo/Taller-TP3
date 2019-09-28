26087dc82e740ce50a6dece3a316f66ca65f07df
alejoacevedo <ale.acevedo@live.com.ar>
2019-09-28 14:29:53 -0300

    Add some test in main

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "honey_pot.h"
+#include "file_error.h"
+#include "key_not_found_error.h"
+
+int main(int argc, char* argv[]) {
+  try {
+    HoneyPot pot("entradas/config.cfg");
+    pot.load_confs();
+    std::cout << pot.get_current_dir();
+    std::cout << pot.get_msg_unknown_cmd();
+  } catch (FileError &e) {
+    std::cout << e.what();
+  } catch (KeyNotFoundError &e) {
+    std::cout << e.what();
+  }
+}
