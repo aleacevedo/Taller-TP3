1bcaca04e50fe5dcf4bf810fb421bdeb86415c20
alejoacevedo <ale.acevedo@live.com.ar>
2019-09-28 14:29:39 -0300

    Add some key definitions

---
+++ key_def.h
@@ -0,0 +1,22 @@
+#ifndef _KEY_DEF_H_
+#define _KEY_DEF_H_
+
+#define USER_KEY "user"
+#define PASS_KEY "password"
+#define MSG_NEW_CLIENT_KEY "newClient"
+#define MSG_PASS_REQUIRED_KEY "passRequired"
+#define MSG_LOGIN_FAIL_KEY "loginFailed"
+#define MSG_LOGIN_SUCC_KEY "loginSuccess"
+#define MSG_NOT_LOG_KEY "clientNotLogged"
+#define CURRENT_DIR_KEY "currentDirectoryMsg"
+#define MSG_LIST_BEGIN_KEY "listBegin"
+#define MSG_LIST_END_KEY "listEnd"
+#define MSG_MKD_SUCC_KEY "mkdSuccess"
+#define MSG_MKD_FAIL_KEY "mkdFailed"
+#define MSG_RMD_SUCC_KEY "rmdSuccess"
+#define MSG_RMD_FAIL_KEY "rmdFailed"
+#define MSG_QUIT_KEY "quitSuccess"
+#define MSG_UNKNOW_KEY "unknownCommand"
+#define SYS_INFO_KEY "systemInfo"
+
+#endif  //  _KEY_DEF_H_
