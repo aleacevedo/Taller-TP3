6419c5da98182bf0cfa97ee7dcf0285bda0029c1
alejoacevedo <ale.acevedo@live.com.ar>
2019-09-29 17:15:25 -0300

    Add some test

--- main.cpp
+++ main.cpp
@@ -4,22 +4,22 @@
 #include "directory.h"
 #include "custom_errors.h"
 #include "socket.h"
-#include "ftp.h"
+#include "command.h"
 
 int main(int argc, char* argv[]) {
-  FTP ftp_pot("entradas/config.cfg");
-  std::cout << ftp_pot.syst(1) << "\n";
-  std::cout << ftp_pot.user(1, "admin") << "\n";
-  std::cout << ftp_pot.pass(1, "l33tP4s5") << "\n";
-  std::cout << ftp_pot.syst(1) << "\n";
-  std::cout << ftp_pot.list(1) << "\n";
-  std::cout << ftp_pot.mkd(1, "newDir1") << "\n";
-  std::cout << ftp_pot.mkd(1, "newDir1") << "\n";
-  std::cout << ftp_pot.mkd(1, "newDir2") << "\n";
-  std::cout << ftp_pot.list(1) << "\n";
-  std::cout << ftp_pot.rmd(1, "newDir1") << "\n";
-  std::cout << ftp_pot.rmd(1, "newDir1") << "\n";
-  std::cout << ftp_pot.list(1) << "\n";
+  FactoryCommand fact("entradas/config.cfg");
+  std::cout << fact.getCommands()["SYST"]->execute(1, "") << "\n";
+  std::cout << fact.getCommands()["USER"]->execute(1, "admin") << "\n";
+  std::cout << fact.getCommands()["PASS"]->execute(1, "l33tP4s5") << "\n";
+  std::cout << fact.getCommands()["SYST"]->execute(1, "") << "\n";
+  std::cout << fact.getCommands()["LIST"]->execute(1, "") << "\n";
+  std::cout << fact.getCommands()["MKD"]->execute(1, "newDir1") << "\n";
+  std::cout << fact.getCommands()["MKD"]->execute(1, "newDir1") << "\n";
+  std::cout << fact.getCommands()["MKD"]->execute(1, "newDir2") << "\n";
+  std::cout << fact.getCommands()["LIST"]->execute(1, "") << "\n";
+  std::cout << fact.getCommands()["RMD"]->execute(1, "newDir1") << "\n";
+  std::cout << fact.getCommands()["RMD"]->execute(1, "newDir1") << "\n";
+  std::cout << fact.getCommands()["LIST"]->execute(1, "") << "\n";
   try {
   } catch (FileError &e) {
     std::cout << e.what();
