a81510e81aeb1f70959f0c79c40831d6a515c3c3
alejoacevedo <ale.acevedo@live.com.ar>
2019-09-28 21:19:50 -0300

    Add test

--- main.cpp
+++ main.cpp
@@ -3,15 +3,21 @@
 #include "honey_pot.h"
 #include "directory.h"
 #include "custom_errors.h"
+#include "socket.h"
 
 int main(int argc, char* argv[]) {
+  std::string receive;
   try {
-    Directory dir;
-    dir.add("First directory");
-    dir.add("Second directory");
-    std::cout << dir.list();
-    dir.remove("First dire ctory");
-    std::cout << dir.list();
+    std::cout << "Empiezo \n";
+    Socket skt("8182");
+        std::cout << "Inicializado \n";
+
+    skt.to_listen();
+        std::cout << "Escuchando \n";
+
+    skt.to_accept();
+    skt.to_receive(receive, 10);
+    skt.to_send(receive, 10);
   } catch (FileError &e) {
     std::cout << e.what();
   } catch (KeyNotFoundError &e) {
