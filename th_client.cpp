b2d926eb3330919cfb60d67c89ec8857ba8d34ac
alejoacevedo <ale.acevedo@live.com.ar>
2019-10-01 10:58:01 -0300

    Fix THClient class

--- th_client.cpp
+++ th_client.cpp
@@ -1,39 +1,53 @@
 #include "th_client.h"
 #include <string>
+#include <exception>
 
 THClient::THClient(int client,
-                   FTP &ftp,
                    FactoryCommand &factory,
-                   Socket &skt) : keep_talking(true),
-                                  client(client),
-                                  ftp(ftp),
-                                  factory(factory),
-                                  skt(skt),
-                                  protocol(skt) {}
+                   Protocol &protocol) : keep_talking(true),
+                                         client(client),
+                                         factory(factory),
+                                         protocol(protocol) {
+}
 
 void THClient::run() {
-  this->protocol.send(this->client, this->ftp.newClient());
-  while (keep_talking) {
-    std::string cmd;
-    if (this->protocol.receive(this->client, cmd) == 0)
-      return;
-    if (this->protocol.send(this->client, this->execute_command(cmd)) == 0)
-      return;
+  try {
+    this->factory.getCommands().at("NEWCLIENT")->execute(this->client, "");
+    while (this->keep_talking) {
+      std::string cmd;
+      if (this->protocol.receive(this->client, cmd) == 0)
+        this->keep_talking = false;
+      if (this->execute_command(cmd) == 0)
+        this->keep_talking = false;
+    }
+  } catch (std::exception &e) {
+    std::cout << "ERROR EN EL THCLIENT " << this->client << "\n";
+    std::cout << e.what();
+  } catch (...) {
+    std::cout << "UNKNOW ERROR\n";
   }
+}
+
+bool THClient::is_keep_talking() {
+  return this->keep_talking;
+}
+
+void THClient::stop() {
+  this->keep_talking = false;
+  this->protocol.quit(this->client);
 }
 
 THClient::~THClient() {}
 
-std::string THClient::execute_command(std::string received) {
+int THClient::execute_command(std::string received) {
   std::string arg = "";
-  std::cout << received << "\n";
   size_t space_pos = received.find(" ");
-  std::cout << space_pos << "\n";
   std::string cmd = received.substr(0, space_pos);
   if (space_pos != std::string::npos)
     arg = received.substr(space_pos+1);
-  std::cout << received << "\n";
-  std::cout << cmd << "\n";
-  std::cout << arg << "\n";
-  return this->factory.getCommands()[cmd]->execute(this->client, arg);
+  try {
+    return this->factory.getCommands().at(cmd)->execute(this->client, arg);
+  } catch (std::exception &e)  {
+    return this->factory.getCommands().at("UNKNOW")->execute(this->client, arg);
+  }
 }
--- th_client.h
+++ th_client.h
@@ -1,29 +1,28 @@
 #ifndef TH_CLIENT_H_
 #define TH_CLIENT_H_
 
-#include <thread>
 #include <string>
+#include "thread.h"
 #include "ftp.h"
 #include "command.h"
 #include "socket.h"
 #include "protocol.h"
 
 
-class THClient : public std::thread {
+class THClient : public Thread {
   bool keep_talking;
   int client;
-  FTP &ftp;
   FactoryCommand &factory;
-  Socket &skt;
-  Protocol protocol;
+  Protocol &protocol;
 
  public:
-  explicit THClient(int client, FTP &ftp, FactoryCommand &factory, Socket &skt);
+  explicit THClient(int client, FactoryCommand &factory, Protocol &protocol);
   virtual void run();
   void stop();
+  bool is_keep_talking();
   virtual ~THClient();
  private:
-  std::string execute_command(std::string cmd);
+  int execute_command(std::string cmd);
 };
 
 #endif  //  TH_CLIENT_H_
