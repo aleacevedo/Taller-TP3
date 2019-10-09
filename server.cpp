0740bf47f34f92728f8c64a8982c768dfd360a37
aleacevedo <ale.acevedo@live.com.ar>
2019-10-09 15:04:06 -0300

    Some test for server

--- server.cpp
+++ server.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
-#include "server_th_server.h"
 #include "common_custom_errors.h"
+#include "server_ftp_factory.h"
+#include "server_ftp.h"
 
 std::string  obtain_input() {
   std::string input;
@@ -11,24 +12,7 @@ std::string  obtain_input() {
 }
 
 int main(int argc, char* argv[]) {
-  try {
-    THServer th("entradas/config.cfg", "81");
-    th.start();
-    while (obtain_input().compare("q") != 0) {}
-    th.stop();
-    th.join();
-  } catch (FileError &e) {
-    std::cout << e.what();
-  } catch (KeyNotFoundError &e) {
-    std::cout << e.what();
-  } catch(DirNotExistError &e) {
-    std::cout << e.what();
-  } catch(DirExistError &e) {
-    std::cout << e.what();
-  } catch(std::exception &e) {
-    std::cout << "ERROR EN EL MAIN \n";
-    std::cout << e.what();
-  } catch(...) {
-    std::cout << "Unknown Error\n";
-  }
+  FTPFactory ftpFact("entradas/config.cfg");
+  FTP ftp(ftpFact.generateFTP());
+  std::cout << ftp.execute("USER admin") << "\n";
 }
