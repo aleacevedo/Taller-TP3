14a6cd875f2dc066918490fcfa299af0aef56b15
aleacevedo <ale.acevedo@live.com.ar>
2019-10-09 15:03:54 -0300

    Refactor server_ftp

--- server_ftp.cpp
+++ server_ftp.cpp
@@ -1,118 +1,18 @@
 #include "server_ftp.h"
-#include <iostream>
-#include <exception>
-#include "common_custom_errors.h"
 
-FTP::FTP(std::string conf_file_path,
-         Protocol &protocol) : myPot(conf_file_path),
-                               myDir(),
-                               logged(),
-                               protocol(protocol) {}
 
-int FTP::newClient(int client) {
-  std::string msg = this->myPot.get_msg_new_client() + "\n";
-  return this->protocol.send(client, msg);
+FTP::FTP(CommandFactory& commandFactory) : auth(0),
+                                           myCommands(
+                                              commandFactory.generateCommands(
+                                                this->auth)) {}
+
+FTP::FTP(FTP&& other) : auth(other.auth),
+                        myCommands(std::move(other.myCommands)) {}
+
+std::string FTP::execute(std::string received) {
+  return this->myCommands->getCommands(received)->execute(received);
 }
 
-int FTP::user(int client, std::string user) {
-  std::string msg;
-  if (this->logged.find(client) != this->logged.end()) {
-    msg = this->myPot.get_msg_pass_required() + "\n";
-  } else if (this->myPot.get_user().compare(user) != 0) {
-    msg = this->myPot.get_msg_not_logged() + "\n";
-  } else {
-    this->logged[client] = false;
-    msg = this->myPot.get_msg_pass_required() + "\n";
-  }
-  return this->protocol.send(client, msg);
-}
-
-int FTP::pass(int client, std::string pass) {
-  std::string msg;
-  if (this->logged.find(client) == this->logged.end()) {
-    msg = this->myPot.get_msg_login_fail() + "\n";
-  } else if (this->myPot.get_password().compare(pass) != 0) {
-    msg = this->myPot.get_msg_login_fail() + "\n";
-  } else {
-    this->logged[client] = true;
-    msg = this->myPot.get_msg_login_success() + "\n";
-  }
-  return this->protocol.send(client, msg);
-}
-
-int FTP::syst(int client) {
-  std::string msg;
-  if (!this->is_logged(client))
-    msg = this->myPot.get_msg_not_logged() + "\n";
-  else
-    msg = this->myPot.get_system_info() + "\n";
-  return this->protocol.send(client, msg);
-}
-
-int FTP::list(int client) {
-  std::string msg;
-  if (!this->is_logged(client)) {
-    msg =  this->myPot.get_msg_not_logged() + "\n";
-  } else {
-    msg = this->myPot.get_msg_list_begin() + "\n";
-    msg += this->myDir.list();
-    msg += this->myPot.get_msg_list_end() + "\n";
-  }
-  return this->protocol.send(client, msg);
-}
-
-int FTP::pwd(int client) {
-  std::string msg;
-  if (!this->is_logged(client))
-    msg = this->myPot.get_msg_not_logged() + "\n";
-  else
-    msg = this->myPot.get_current_dir() + "\n";
-  return this->protocol.send(client, msg);
-}
-
-int FTP::mkd(int client, std::string dir) {
-  std::string msg;
-  if (!this->is_logged(client))
-    msg = this->myPot.get_msg_not_logged() + "\n";
-  try {
-    this->myDir.add(FAKE_INFO + dir);
-    msg = this->myPot.get_msg_mkd_success() + " " + dir + "\n";
-  } catch(DirExistError &e) {
-    msg = this->myPot.get_msg_mkd_fail() + "\n";
-  }
-  return this->protocol.send(client, msg);
-}
-
-int FTP::rmd(int client, std::string dir) {
-  std::string msg;
-  if (!this->is_logged(client))
-    msg = this->myPot.get_msg_not_logged() + "\n";
-  try {
-    this->myDir.remove(FAKE_INFO + dir);
-    msg = this->myPot.get_msg_rmd_success() + "\n";
-  } catch(DirNotExistError &e) {
-    msg = this->myPot.get_msg_rmd_fail() + "\n";
-  }
-  return this->protocol.send(client, msg);
-}
-
-int FTP::unknow(int client) {
-  std::string msg = this->myPot.get_msg_unknown_cmd() + "\n";
-  return this->protocol.send(client, msg);
-}
-
-int FTP::quit(int client) {
-  if (this->logged.find(client) != this->logged.end())
-    this->logged.erase(client);
-  std::string msg = this->myPot.get_msg_quit();
-  this->protocol.send(client, msg);
-  this->protocol.quit(client);
-  return 0;
-}
-
-FTP::~FTP() {}
-
-bool FTP::is_logged(int client) {
-  return this->logged.find(client) != this->logged.end()
-         && this->logged[client];
+FTP::~FTP() {
+  delete myCommands;
 }
--- server_ftp.h
+++ server_ftp.h
@@ -3,32 +3,18 @@
 
 #include <map>
 #include <string>
-#include "server_honey_pot.h"
-#include "server_directory.h"
-#include "common_protocol.h"
+#include "server_command_factory.h"
+#include "server_command.h"
 
-#define FAKE_INFO "drwxrwxrwx 0 1000 1000 4096 Sep 24 12:34 "
 
 class FTP {
-  HoneyPot myPot;
-  Directory myDir;
-  std::map<int, bool> logged;
-  Protocol &protocol;
+  int auth;
+  AllCommands *myCommands;
  public:
-  explicit FTP(std::string conf_file_pahth, Protocol &protocol);
-  int newClient(int client);
-  int user(int client, std::string user);
-  int pass(int client, std::string pass);
-  int syst(int client);
-  int list(int cient);
-  int pwd(int client);
-  int mkd(int client, std::string dir);
-  int rmd(int client, std::string dir);
-  int unknow(int client);
-  int quit(int client);
+  explicit FTP(CommandFactory &commandFactory);
+  FTP(FTP&& FTP);
+  std::string execute(std::string received);
   ~FTP();
- private:
-  bool is_logged(int client);
 };
 
 #endif  //  FTP_H_
