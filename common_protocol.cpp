d2651a17dcee3f2d6e448a74826411e2772dc793
aleacevedo <ale.acevedo@live.com.ar>
2019-10-10 17:51:46 -0300

    Refactor protocol receive

--- common_protocol.cpp
+++ common_protocol.cpp
@@ -4,16 +4,18 @@
 Protocol::Protocol(Socket &skt) : skt(skt) {}
 
 int Protocol::send(std::string msg) {
+  msg += "\n";
   return this->skt.to_send(msg, msg.size());
 }
 
 int Protocol::receive(std::string &received) {
   std::string caracter;
   this->skt.to_receive(caracter, 1);
-  while (caracter.compare("\n") != 0 && caracter.compare("\r") != 0) {
-    received = received + caracter;
-    if (this->skt.to_receive(caracter, 1) == 0)
+  while (caracter.compare("\n") != 0) {
+      received = received + caracter;
+    if (this->skt.to_receive(caracter, 1) == 0) {
       return 0;
+    }
   }
   return 1;
 }
