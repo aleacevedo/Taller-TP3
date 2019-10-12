79ec70e485991aca70ebd7c2a9c279e6a5b8b29a
alejoacevedo <ale.acevedo@live.com.ar>
2019-10-12 10:37:14 -0300

    Snake case

--- server_ftp.cpp
+++ server_ftp.cpp
@@ -2,17 +2,21 @@
 
 
 FTP::FTP(CommandFactory& commandFactory) : auth(new int(0)),
-                                           myCommands(
-                                              commandFactory.generateCommands(
+                                           my_commands(
+                                              commandFactory.generate_commands(
                                                 this->auth)) {}
 
 FTP::FTP(FTP&& other) : auth(other.auth),
-                        myCommands(std::move(other.myCommands)) {
+                        my_commands(std::move(other.my_commands)) {
   other.auth = nullptr;
 }
 
+std::string FTP::welcome() {
+  return this->my_commands.get_welcome()->execute("");
+}
+
 std::string FTP::execute(std::string received) {
-  return this->myCommands.getCommands(received)->execute(received);
+  return this->my_commands.get_commands(received)->execute(received);
 }
 
 FTP::~FTP() {
